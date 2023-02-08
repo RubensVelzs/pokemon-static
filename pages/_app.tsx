2a52df59399d3e8ce5c154cb7e872430c9844fae
Ruben Velazquez <rvelazquez@arkangeles.com>
2023-02-08 11:03:22 -0700

    Upload hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react';
 import '../styles/globals.css';
 import {darkTheme} from '../themes/darkTheme';
+import Script from 'next/script';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider theme={darkTheme}>
+        <Script src="../api/hotjar"/>
         <Component {...pageProps} />
     </NextUIProvider>
   );
