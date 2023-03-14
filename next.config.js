4b88260850c61be5dc9043f10a7dd496e4af8bf5
Ruben Velazquez <rvelazquez@arkangeles.com>
2023-03-14 15:19:23 -0700

    Adding security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig:{
@@ -7,7 +15,15 @@ const nextConfig = {
   },
   images:{
     domains:["raw.githubusercontent.com"]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
