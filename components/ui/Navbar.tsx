9566ce81436f059a7a8e29043299a94762b8819d
Ruben Velazquez <rvelazquez@arkangeles.com>
2023-05-31 12:45:33 -0700

    Revert "restore"
    This reverts commit 71599b005e06fa707b40002d627d8c7ff07bf585.

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -14,14 +14,14 @@ export const Navbar:React.FC = ():JSX.Element => {
         padding:"0px 20px",
         backgroundColor:theme?.colors.gray100.value
     }}>
-        <Image alt="App icon" height={70} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" width={70}/> 
+       {/*  <Image alt="App icon" height={70} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" width={70}/> */}
 
   
           <Link href="/">
          
          
-           <Text color='white' h2>P</Text>
-            <Text color='white' h3>okémons</Text>
+           {/*  <Text color='white' h2>P</Text>
+            <Text color='white' h3>okémons</Text> */}
           </Link>
 
         
